1cc177226b83a94dcbf629728f89c01ee88d1f89
ThomasIVM <tmeijer@ictvanafmorgen.nl>
2024-05-08 12:23:09 +0200

    styling update logout buttons to confirm you want to logout

--- react-realtime-firebase/src/components/logout.js
+++ react-realtime-firebase/src/components/logout.js
@@ -1,22 +1,44 @@
+import React, { useState } from "react";
 import { signOut } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../config/firebase";
 
 const Logout = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [show, setShow] = useState(true);
 
-    try {
-        // Using Firebase function to sign out the user
-         signOut(auth);
-        localStorage.setItem('auth', '');
-        // setTimeout(function(){
-          navigate('/');
-          window.location.reload();
-      //  }, 2500);
-      } catch (err) {
-        // Handling errors, if any
-        console.error(err.message);
-      }
-}
+  const handleClose = () => {
+    // Prevent the user from closing the modal
+  };
 
-export default Logout
+  return (
+    <div className="logout">
+      <div className="logoutField" show={show} onHide={handleClose} backdrop="static" keyboard={false}>
+        <div className="logoutText">Weet je zeker dat je wil uitloggen </div>
+        <div className="logOutButtons">
+          <button className="Yep"
+            variant="primary"
+            onClick={() => {
+              signOut(auth);
+              localStorage.setItem("auth", "");
+              navigate("/");
+              window.location.reload();
+            }}
+          >
+            Ja
+          </button>
+          <button className="Nope"
+            variant="secondary"
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            Nee
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logout;
